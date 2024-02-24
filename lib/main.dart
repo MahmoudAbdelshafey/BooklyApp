a742a928daf86afc1d75d803276f949b452d3e57
MahmoudAbdelshafey <Mahmoud.abdelshafey@stemluxor.moe.edu.eg>
2024-02-24 19:18:29 +0200

    Bookly App Setup second

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(BooklyApp());
+}
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
